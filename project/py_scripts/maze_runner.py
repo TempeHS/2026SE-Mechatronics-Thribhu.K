d21408d1c76a8118cf806e08ff7bcf25cccd59d3
tk <4tkbytes@pm.me>
2025-08-08 10:02:31 +1000

    async works now 👅👅👅

--- project/py_scripts/maze_runner.py
+++ project/py_scripts/maze_runner.py
@@ -1,14 +1,17 @@
 from servo import Servo
 from machine import Pin, PWM
 from PiicoDev_VEML6040 import PiicoDev_VEML6040
+from PiicoDev_Ultrasonic import PiicoDev_Ultrasonic
 import time
+import asyncio
 
 print("Running maze runner")
 
 # a monovar that describes the debug scopes. can easily change it. 
 debug_scope: dict[str, bool] = {
     "main": True,
-    "wheel": True
+    "wheel": True,
+    "light": True
 }
 
 class WheelGroup:
@@ -35,7 +38,7 @@ class WheelGroup:
         if speed > 1.0 or speed < -1.0:
             raise ValueError("Speed percentage must be between -1.0 and 1.0, 0.0 being stop")
         self.__lw.set_duty(percentage_to_duty(speed))
-        self.__rw.set_duty(percentage_to_duty(speed))
+        self.__rw.set_duty(percentage_to_duty(speed * -1))
         
         if self.__debug: print(f"Duty: {self.__lw.get_duty()} {self.__rw.get_duty()}")
     
@@ -75,35 +78,85 @@ def percentage_to_duty(percentage: float) -> int:
     return duty
 
 class DiChromaticLightSensor:
-    def __init__(self):
+    def __init__(self, debug: bool):
         self.__sensor = PiicoDev_VEML6040()
-        # figure out what colours black and white are (the hues)
-        self.__black = 0
-        self.__white = 0
-        # todo: get this to work
-        # self.__current_light = self.__sensor.
+        self.__debug = debug
+        self.__current_light = self.__sensor.readRGB()
+    
+    def debug(self):
+        if self.__debug:
+            print(str(self.__current_light["blue"]) + " Blue  " + str(self.__current_light["green"]) + " Green  " + str(self.__current_light["red"]) + " Red")
+    
+    def update(self):
+        self.__current_light = self.__sensor.readRGB()
     
     def is_black(self) -> bool:
         """
         Returns a boolean if the light sensor detects a colour that is black
-        or is of the shade of black
+        or is of the shade of black. 
+        
+        If the green value (detected by the sensor) is less than 200, then it
+        is considered as black. 
         """
-        pass
+        # todo: add race conditions for other colours
+        self.update()
+        if self.__current_light["green"] < 500:
+            if self.__debug: print("Found black value")
+            return True
+        else:
+            return False
 
     def is_white(self) -> bool:
         """
         Returns a boolean if the light sensor detects a colour that is white
         or is of the shade of white
+        
+        If the green value (detected by the sensor) is more than 5000, then it is considered as black. 
         """
-        pass
+        # todo: add race conditions for other colours
+        self.update()
+        if self.__current_light["green"] > 5500:
+            if self.__debug: print("Found white value")
+            return True
+        else:
+            return False
+
+# class DualUltrasonicSensorGroup:
+#     def __init__(self, sensors: dict[str, list[int]]):
+        
+
+# sensors = DualUltrasonicSensorGroup()
 
 wheel_group = WheelGroup(20, 16, debug_scope["wheel"])
+light_sensor = DiChromaticLightSensor(debug_scope["light"])
 
 main_debug = debug_scope["main"]
 
-while True:
-    # wheel_group.move_forward(0.25)
-    wheel_group.move_left(0.5)
-    time.sleep(2)
-    wheel_group.stop()
-    time.sleep(2)
+stop_signal = False
+
+async def wheel_task():
+    global stop_signal
+    while True:
+        if not stop_signal:
+            wheel_group.move_forward(0.5)
+        else:
+            wheel_group.stop()
+        await asyncio.sleep(0.5)
+
+async def sensor_task():
+    global stop_signal
+    while True:
+        if light_sensor.is_white():
+            print("White detected — stopping wheels")
+            stop_signal = True
+        else:
+            stop_signal = False
+        await asyncio.sleep(0.2)
+
+async def main():
+    await asyncio.gather(
+        wheel_task(),
+        sensor_task()
+    )
+
+asyncio.run(main())
